ba6c690832b9d4ef49897c428f8c03e5a4b02077
LordQuantum <69377613+LordQuantum@users.noreply.github.com>
2023-05-27 23:13:09 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
   VITE_CORS_PROXY_URL: "https://movies.svibudha7114252.workers.dev",
-  VITE_TMDB_API_KEY: "b030404650f279792a8d3287232358e3",
+  VITE_TMDB_API_KEY: "5d4114c542ee8bc4e5c6f38f5f2306cb",
   VITE_OMDB_API_KEY: "aa0937c0",
 };
