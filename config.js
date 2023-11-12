da4fbe0987931413b8da0f3b3a2a04c08ea590df
LordQuantum <69377613+LordQuantum@users.noreply.github.com>
2023-11-12 22:30:54 +1100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
   VITE_CORS_PROXY_URL: "https://movies.svibudha7114252.workers.dev",
-  VITE_TMDB_READ_API_KEY: "449fd7071217e6342817cb4a4b50dac5"
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NDlmZDcwNzEyMTdlNjM0MjgxN2NiNGE0YjUwZGFjNSIsInN1YiI6IjY0NmNiN2QzYTUwNDZlMDEwNThkMTY3MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.croy_S8S0nDsfcj2q9j2E2PS0qF6oavDp9SL8XGg1Os"
 };
