7772a66aba63ced510cef1b02b274d02d4b3802e
LordQuantum <69377613+LordQuantum@users.noreply.github.com>
2023-11-12 22:24:17 +1100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://movies.svibudha7114252.workers.dev",
+  VITE_TMDB_READ_API_KEY: "5d4114c542ee8bc4e5c6f38f5f2306cb"
+};
