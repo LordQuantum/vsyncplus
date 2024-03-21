b1a126dce79e51a92d2f1d7ccf3f38847a50b013
LordQuantum <69377613+LordQuantum@users.noreply.github.com>
2024-03-21 21:29:58 +1100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "https://movies.svibudha7114252.workers.dev",
+  VITE_CORS_PROXY_URL: "https://app.netlify.com/sites/creative-florentine-0df8d5/deploys/65fc0b344405a8aa66ae5acd",
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NDlmZDcwNzEyMTdlNjM0MjgxN2NiNGE0YjUwZGFjNSIsInN1YiI6IjY0NmNiN2QzYTUwNDZlMDEwNThkMTY3MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.croy_S8S0nDsfcj2q9j2E2PS0qF6oavDp9SL8XGg1Os"
 };
