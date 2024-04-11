5b57d4e151d7218f66d818d3e7b6820e761f173d
LordQuantum <69377613+LordQuantum@users.noreply.github.com>
2024-04-11 23:03:39 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "https://creative-florentine-0df8d5.netlify.app",
+  VITE_CORS_PROXY_URL: "https://mw-backend.lonelil.com",
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NDlmZDcwNzEyMTdlNjM0MjgxN2NiNGE0YjUwZGFjNSIsInN1YiI6IjY0NmNiN2QzYTUwNDZlMDEwNThkMTY3MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.croy_S8S0nDsfcj2q9j2E2PS0qF6oavDp9SL8XGg1Os"
 };
